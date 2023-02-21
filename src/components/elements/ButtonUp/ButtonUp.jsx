30b0f6119f6fe12219b200b2cef2732c86f8282f
Yevheniia Bohdan <AlmDomini@gmail.com>
2023-02-21 19:08:43 +0100

    wow

--- src/components/elements/ButtonUp/ButtonUp.jsx
+++
@@ -1,9 +0,0 @@
-import { addBackToTop } from 'vanilla-back-to-top';
-
-export const ButtonUp = () => {
-  return addBackToTop({
-    diameter: 40,
-    backgroundColor: '#ddd',
-    textColor: 'red',
-  });
-};
